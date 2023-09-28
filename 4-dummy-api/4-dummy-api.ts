e7e23cd1950a4131701deeca6fe3afabfbd0ba70
Roman Larionov <larionovra@gmail.com>
2023-09-28 21:21:21 +0300

    fix before comment

--- 4-dummy-api/4-dummy-api.ts
+++ 4-dummy-api/4-dummy-api.ts
@@ -2,91 +2,91 @@ import fetch from 'node-fetch'
 
 enum Gender {
     MALE = 'male',
-    FEMALE = 'female'
+    FEMALE = 'female',
 }
 
 enum HairColor {
     BLACK = 'Black',
-    CHESTNUT = 'Chestnut'
+    CHESTNUT = 'Chestnut',
 }
 
 enum HairType {
     STRANDS = 'Strands',
-    WAVY = 'Wavy'
+    WAVY = 'Wavy',
 }
 
 enum EyeColor {
     BLUE = 'Blue',
-    GREEN = 'Green'
+    GREEN = 'Green',
 }
 
 interface IAddress {
-    address: string,
-    city: string,
+    address: string;
+    city: string;
     coordinates: {
-        lat: number,
-        lng: number
+        lat: number;
+        lng: number;
     },
-    postalCode: string,
-    state: string
+    postalCode: string;
+    state: string;
 }
 
 interface IHair {
-    color: HairColor,
-    type: HairType
+    color: HairColor;
+    type: HairType;
 }
 
 
 interface IUser {
-    id: number,
-    firstName: string,
-    lastName: string,
-    maidenName: string,
-    age: number,
-    gender: Gender,
-    email: string,
-    phone: string,
-    username: string,
-    password: string,
-    birthDate: string,
-    image: string,
-    bloodGroup: string,
-    height: number,
-    weight: number,
-    eyeColor: EyeColor,
-    hair: IHair,
-    domain: string,
-    ip: string,
-    address: IAddress,
-    macAddress: string,
-    university: string,
+    id: number;
+    firstName: string;
+    lastName: string;
+    maidenName: string;
+    age: number;
+    gender: Gender;
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    birthDate: string;
+    image: string;
+    bloodGroup: string;
+    height: number;
+    weight: number;
+    eyeColor: EyeColor;
+    hair: IHair;
+    domain: string;
+    ip: string;
+    address: IAddress;
+    macAddress: string;
+    university: string;
     bank: {
-        cardExpire: string,
-        cardNumber: string,
-        cardType: string,
-        currency: string,
-        iban: string
+        cardExpire: string;
+        cardNumber: string;
+        cardType: string;
+        currency: string;
+        iban: string;
     },
     company: {
-        address: IAddress,
-        department: string,
-        name: string,
-        title: string
+        address: IAddress;
+        department: string;
+        name: string;
+        title: string;
     },
-    ein: string,
-    ssn: string,
-    userAgent: string
+    ein: string;
+    ssn: string;
+    userAgent: string;
 }
 
 interface IResponseSuccess {
-    users: IUser[],
-    total: number,
-    skip: number,
-    limit: number
+    users: IUser[];
+    total: number;
+    skip: number;
+    limit: number;
 }
 
 interface IResponseFailed {
-    message: string
+    message: string;
 }
 
 async function getData(): Promise<void | undefined> {

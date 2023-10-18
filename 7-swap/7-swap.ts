ff2d5c8a0cc60e76ea33dc5764e5d560d060d87c
Roman Larionov <larionovra@gmail.com>
2023-10-18 17:58:08 +0300

    add 7-swap change 1

--- 7-swap/7-swap.ts
+++ 7-swap/7-swap.ts
@@ -3,8 +3,10 @@ const obj: Record<string, number> = {
     b: 2
 }
 
-function swapKeysAndValues <T extends string, N>(obj: Record<T, N>): Record<T, T> | {} {
-    let result = {}
+const obj2 ={}
+
+function swapKeysAndValues <T extends string, N>(obj: Record<T, N>): Record<T, T> {
+    let result: Record<T, T> = {}
    for (let [k, v] of Object.entries(obj)) {
        const newV = String(v)
        result[newV] = k
@@ -13,5 +15,7 @@ function swapKeysAndValues <T extends string, N>(obj: Record<T, N>): Record<T, T
 }
 
 const res = swapKeysAndValues<string, number>(obj)
+const res2 = swapKeysAndValues<string, number>(obj2)
 
 console.log(res)
+console.log(res2)

8accc55dfa17ebe42a096b18235513d7b9b9dca3
Roman Larionov <larionovra@gmail.com>
2023-10-23 21:21:38 +0300

    add 9-difference

--- 9-difference/9-difference.ts
+++ 9-difference/9-difference.ts
@@ -11,19 +11,25 @@ interface IB {
 let a: IA = { a: 5, b: '' }
 let b: IB = { a: 10, c: true }
 
-type IDifference = Omit<IA, keyof IB>
+interface IDifference {
+    b: string
+}
+type TDifference = Omit<IA, keyof IB>
+type TExclude = Exclude<keyof IA, keyof IB>
+type TDifference2 = Pick<IA, TExclude>
 
-function difference(obj1: IA, obj2: IB): IDifference {
+function difference(obj1, obj2) {
     let res = obj1
-
     for(let key in obj2) {
         if(key in obj1) {
             delete res[key]
         }
     }
-
     return res
 }
 
 let v0: IDifference = difference(a, b)
+let v1: TDifference2 = difference(a, b)
+
 console.log(v0)
+console.log(v1)

b5f0a0c28d592e7bcf0e988e5904059533badba3
Roman Larionov <larionovra@gmail.com>
2023-11-09 22:24:31 +0300

    fix

--- 9-difference/9-difference.ts
+++ 9-difference/9-difference.ts
@@ -14,11 +14,11 @@ let b: IB = { a: 10, c: true }
 interface IDifference {
     b: string
 }
-type TDifference = Omit<IA, keyof IB>
-type TDifference2<A extends {}, B extends {}> = Pick<IA, Exclude<keyof A, keyof B>>
+// type TDifference = Omit<IA, keyof IB>
+type TDifference2<A extends {}, B extends {}> = Pick<A, Exclude<keyof A, keyof B>>
 
 function difference<A extends {}, B extends {}>(obj1: A, obj2: B): TDifference2<A, B> {
-    let res: A = obj1
+    let res: TDifference2<A, B> = obj1 as TDifference2<A, B>
     for(let key in obj2) {
         if(key in obj1) {
             delete res[key]
@@ -27,6 +27,6 @@ function difference<A extends {}, B extends {}>(obj1: A, obj2: B): TDifference2<
     return res
 }
 
-let v1: TDifference2<IA, IB> = difference(a, b)
+let v1 = difference(a, b)
 
 console.log(v1)
